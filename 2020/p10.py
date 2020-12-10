594f7d9307032bed036b81a045b486e4359e6b51
Christian Nash <cnash3712@gmail.com>
2020-12-10 12:18:50 -0500

    cleaner

--- 2020/p10.py
+++ 2020/p10.py
@@ -1,37 +1,12 @@
 #!/usr/bin/env python
 
 from utils import *
-from collections import defaultdict
 from functools import lru_cache
 
 def part_one(inputs):
-    MAX_DIFF = 3
-    adapters = sorted([int(x) for x in inputs])
-    diffs = defaultdict(int)
-
-    prev = 0
-    for adapter in adapters:
-        diffs[adapter-prev]+=1
-        prev = adapter
-    diffs[3]+=1
-
-    return diffs[1] * diffs[3]
-
-# OLD AND GROSS
-# def build_chain(curr, memo, adapters):
-#     if curr in memo:
-#         return memo[curr], memo
-#     if not any([x in adapters for x in range(curr+1, curr+4)]):
-#         return 1, memo
-    
-#     myval = 0
-#     for i,x in enumerate(range(curr+1, curr+4)):
-#         if x in adapters:
-#             childres = build_chain(x, memo, [y for y in adapters if y > x])
-#             myval+= childres[0]
-#             memo = childres[1]
-#     memo[curr] = myval
-#     return myval, memo
+    adapters = [0] + sorted([int(x) for x in inputs])
+    c = [adapters[i]-adapters[i-1] for i in range(1, len(adapters))] + [3]
+    return len([x for x in c if x == 1]) * len([x for x in c if x == 3])
 
 @lru_cache
 def build_chain(curr, adapters):
@@ -44,14 +19,14 @@ def part_two(inputs):
 
 ss = start_time()
 # Part One 
-print(part_one(aocin('inputs/10.1')))
-print(part_one(aocin('inputs/10.2')))
+# print(part_one(aocin('inputs/10.1')))
+# print(part_one(aocin('inputs/10.2')))
 print(part_one(aocin('inputs/10')))
 end_time(ss)
 
 ss = start_time()
 # Part One 
-print(part_two(aocin('inputs/10.1')))
-print(part_two(aocin('inputs/10.2')))
+# print(part_two(aocin('inputs/10.1')))
+# print(part_two(aocin('inputs/10.2')))
 print(part_two(aocin('inputs/10')))
 end_time(ss)
